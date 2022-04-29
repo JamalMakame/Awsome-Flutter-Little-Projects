d5e8f524b6e6442bc7c0c4fc93e36517e5fc8985
Jamal Makame <jamal.abdalla@students.jkuat.ac.ke>
2022-04-29 21:00:52 +0300

    added

--- 04_delivery_app/android/app/src/main/kotlin/com/example/delivery_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.delivery_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ 04_delivery_app/parcel_delivery_app/android/app/src/main/kotlin/com/example/parcel_delivery_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.parcel_delivery_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
