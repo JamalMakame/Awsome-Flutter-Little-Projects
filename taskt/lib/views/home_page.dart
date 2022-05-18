01d9eb38744f851595cce4503391702205138a75
Jamal Makame <jamal.abdalla@students.jkuat.ac.ke>
2022-05-18 23:52:19 +0300

    added

--- taskt/lib/views/home_page.dart
+++ taskt/lib/views/home_page.dart
@@ -1,4 +1,3 @@
-import 'dart:js';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -80,12 +79,13 @@ class HomePage extends StatelessWidget {
               ],
             ),
           ),
-          CustomScrollView(
-            scrollDirection: Axis.horizontal,
-            slivers: [
-              customCards(),
-            ],
-          ),
+          // CustomScrollView(
+          //   //shrinkWrap: true,
+          //   //scrollDirection: Axis.horizontal,
+          //   slivers: [
+          //     customCards(),
+          //   ],
+          // ),
         ],
       ),
     );
@@ -103,59 +103,63 @@ Widget customCards() {
             fit: StackFit.expand,
             children: <Widget>[
               Positioned(
-                  top: 0,
-                  left: 0,
-                  child: SizedBox(
-                      width: 341,
-                      height: 339,
-                      child: Stack(children: <Widget>[
-                        Positioned(
-                          top: 0,
-                          left: 0,
-                          child: Container(
-                            width: 341,
-                            height: 338.35491943359375,
-                            decoration: const BoxDecoration(
-                              borderRadius: BorderRadius.all(
-                                Radius.circular(20),
-                              ),
-                              gradient: LinearGradient(
-                                begin: Alignment(
-                                    0.0374455489218235, 0.7739855647087097),
-                                end: Alignment(
-                                    -0.7739855647087097, 0.06429413706064224),
-                                colors: [
-                                  Color.fromRGBO(156, 44, 243, 1),
-                                  Color.fromRGBO(58, 72, 248, 1)
-                                ],
-                              ),
+                top: 0,
+                left: 0,
+                child: SizedBox(
+                  width: 341,
+                  height: 339,
+                  child: Stack(
+                    fit: StackFit.expand,
+                    children: <Widget>[
+                      Positioned(
+                        top: 0,
+                        left: 0,
+                        child: Container(
+                          width: 341,
+                          height: 338.35491943359375,
+                          decoration: const BoxDecoration(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(20),
+                            ),
+                            gradient: LinearGradient(
+                              begin: Alignment(
+                                  0.0374455489218235, 0.7739855647087097),
+                              end: Alignment(
+                                  -0.7739855647087097, 0.06429413706064224),
+                              colors: [
+                                Color.fromRGBO(156, 44, 243, 1),
+                                Color.fromRGBO(58, 72, 248, 1)
+                              ],
                             ),
                           ),
                         ),
-                        const Positioned(
-                            top: 0.16085033118724823,
-                            left: 0,
-                            child: //Mask holder Template
-                                SizedBox(
-                                    width: 341,
-                                    height: 338.8391418457031,
-                                    child: null)),
-                        const Positioned(
-                            top: 126,
-                            left: 37,
-                            child: Text(
-                              'Front-End Development',
-                              textAlign: TextAlign.left,
-                              style: TextStyle(
-                                  color: Color.fromRGBO(255, 255, 255, 1),
-                                  fontFamily: 'Poppins',
-                                  fontSize: 33,
-                                  letterSpacing: 0,
-                                  fontWeight: FontWeight.normal,
-                                  height: 1.5 /*PERCENT not supported*/
-                                  ),
-                            )),
-                      ]))),
+                      ),
+                      const Positioned(
+                          top: 0.16085033118724823,
+                          left: 0,
+                          child: SizedBox(
+                              width: 341,
+                              height: 338.8391418457031,
+                              child: null)),
+                      const Positioned(
+                        top: 126,
+                        left: 37,
+                        child: Text(
+                          'Front-End Development',
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                              color: Color.fromRGBO(255, 255, 255, 1),
+                              fontFamily: 'Poppins',
+                              fontSize: 33,
+                              letterSpacing: 0,
+                              fontWeight: FontWeight.normal,
+                              height: 1.5),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
               const Positioned(
                 top: 272,
                 left: 37,
