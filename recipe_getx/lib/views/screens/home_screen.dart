2fb72382771e1dd8e1c6c14dabc33d72356270ea
JamalMakame <jamal.abdalla@students.jkuat.ac.ke>
2022-07-10 12:57:51 +0300

    initial commit

---
+++ recipe_getx/lib/views/screens/home_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            
+          ),
+        ],
+      ),
+    );
+  }
+}
