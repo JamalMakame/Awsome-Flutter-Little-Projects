a97c83ccc8da73a55219c9f8580f83ba145c5150
JamalMakame <jamal.abdalla@students.jkuat.ac.ke>
2022-08-19 15:23:40 +0300

    updated the boarding screen

---
+++ 04_tech_ecommerce_app/lib/app/modules/onboarding/views/onboarding_view.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import '../controllers/onboarding_controller.dart';
+
+class OnboardingView extends GetView<OnboardingController> {
+  const OnboardingView({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('OnboardingView'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text(
+          'OnboardingView is working',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
--- 04_tech_ecommerce_app/lib/main.dart
+++ 04_tech_ecommerce_app/lib/main.dart
@@ -1,4 +1,6 @@
+
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'package:get/get.dart';
 
@@ -6,10 +8,16 @@ import 'app/routes/app_pages.dart';
 
 void main() {
   runApp(
-    GetMaterialApp(
-      title: "Application",
-      initialRoute: AppPages.INITIAL,
-      getPages: AppPages.routes,
+    ScreenUtilInit(
+      designSize: const Size(414, 896),
+      minTextAdapt: true,
+      builder: (context, child) {
+        return GetMaterialApp(
+          title: "Application",
+          initialRoute: AppPages.INITIAL,
+          getPages: AppPages.routes,
+        );
+      },
     ),
   );
 }
